c5fa925613fb72d0fa1d907f395a71bca72c343c
stripepython <98206176+stripepython@users.noreply.github.com>
2022-02-07 21:10:49 +0800

    Update timer.py

--- fastgame/utils/timer.py
+++ fastgame/utils/timer.py
@@ -4,11 +4,14 @@ Fastgame计时器模块。
 """
 
 import time
+from typing import Union
 
 
 class Timer(object):
     def __init__(self):
         self.start = time.time()
+        self.on_pause = False
+        self.pause_time = 0
         
     def __float__(self):
         return time.time() - self.start
@@ -27,7 +30,7 @@ class Timer(object):
         :return: 计时器计时值的近似值
         :rtype: float
         """
-        return round(time.time() - self.start, digits)
+        return round(self.get_real(), digits)
     
     def get_real(self):
         """
@@ -36,5 +39,30 @@ class Timer(object):
         :return: 计时器计时值
         :rtype: float
         """
+        if self.on_pause:
+            return self.pause_time
         return time.time() - self.start
+    
+    def pause(self):
+        """
+        暂停计时。
+        """
+        self.pause_time = self.get_real()
+        self.on_pause = True
+        
+    def unpause(self):
+        """
+        继续(取消暂停)计时。
+        """
+        self.on_pause = False
+        self.start = time.time() - self.pause_time
+    
+    @staticmethod
+    def wait(seconds: Union[int, float]):
+        """
+        等待一段时间。
+        
+        :param seconds: 等待时间，单位为秒。
+        """
+        time.sleep(seconds)
     
